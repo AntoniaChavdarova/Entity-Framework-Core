eeb2f46b173d082e83c07395f0f7ded36bfcd19c
AntoniaChavdarova <niaaachavdarova@abv.bg>
2021-03-04 16:26:37 +0200

    Create DbContext

---
+++ PetStoreWorkshop/PetStore/PetStore.Data/Configurations/ClientProductEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+using PetStore.Models;
+
+namespace PetStore.Data.Configurations
+{
+    public class ClientProductEntityConfiguration
+        : IEntityTypeConfiguration<ClientProduct>
+    {
+        public void Configure(EntityTypeBuilder<ClientProduct> builder)
+        {
+            builder
+                .HasKey(cp => new { cp.ClientId, cp.ProductId });
+
+        }
+    }
+}
